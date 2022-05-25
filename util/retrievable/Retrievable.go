4db527768fb42511f13e7bc9873357146fce0618
Luca Weist <lweist3317@gmail.com>
2022-05-25 19:10:39 +0200

    Retievable remake

--- util/retrievable/Retrievable.go
+++ util/retrievable/Retrievable.go
@@ -1,9 +1,5 @@
 package retrievable
 
-// TODO
-// CreateBundle/ReadBundle
-// maybe marshaled25519 instead of the generic marshalling
-
 import (
 	"bytes"
 	"encoding/gob"
@@ -15,11 +11,10 @@ import (
 
 	"github.com/Luca3317/libsignalcopy/ecc"
 	"github.com/Luca3317/libsignalcopy/keys/identity"
-	"github.com/Luca3317/libsignalcopy/logger"
 	"github.com/Luca3317/libsignalcopy/serialize"
 	"github.com/Luca3317/libsignalcopy/state/record"
+	"github.com/Luca3317/libsignalcopy/util/bytehelper"
 	"github.com/Luca3317/libsignalcopy/util/keyhelper"
-	"github.com/libp2p/go-libp2p-core/crypto"
 )
 
 const idspath = "ids.bin"
@@ -44,33 +39,36 @@ type RetrievableRaw struct {
 }
 
 type Retrievable struct {
-	Ids                 IDs
-	PreKey              record.PreKey
-	SignedPreKey        record.SignedPreKey
-	IdentityKeyPairPub  crypto.PubKey
-	IdentityKeyPairPriv crypto.PrivKey
+	Ids             IDs
+	PreKey          record.PreKey
+	SignedPreKey    record.SignedPreKey
+	IdentityKeyPair identity.KeyPair
 }
 
-func writefile(content []byte, path string) {
+func writefile(content []byte, path string) error {
 
 	permissions := 0644
 	err := ioutil.WriteFile(path, content, fs.FileMode(permissions))
 
 	if err != nil {
-		log.Fatalf("FAILED TO WRITE TO FILE %v", path)
 		os.Remove(path)
+		return err
 	}
+
+	return nil
 }
 
-func readfile(content *[]byte, path string) {
+func readfile(content *[]byte, path string) error {
 
 	var err error
 	*content, err = ioutil.ReadFile(path)
 
 	if err != nil {
-		log.Fatalf("FAILED TO READ FROM FILE %v", path)
 		*content = nil
+		return err
 	}
+
+	return nil
 }
 
 func IDsToGOB(id IDs) []byte {
@@ -101,151 +99,6 @@ func InitGOB() {
 	gob.Register(IDs{})
 }
 
-// ID keypairs are handled differently; The go implementations are saved / read
-// To get Signal compatible, equivalent keys, use ConvertIDKeysLibp2pToSig
-func SaveIDKeyPair(pubLibp2p crypto.PubKey, privLibp2p crypto.PrivKey) (pubBytes []byte, privBytes []byte) {
-	pubBytes, err := crypto.MarshalPublicKey(pubLibp2p)
-	privBytes, err2 := crypto.MarshalPrivateKey(privLibp2p)
-	if err != nil {
-		panic(err)
-	} else if err2 != nil {
-		panic(err2)
-	}
-
-	writefile(pubBytes, idkeypubpath)
-	writefile(privBytes, idkeyprivpath)
-	return pubBytes, privBytes
-}
-
-func ConvertIDKeysLibp2pToSig(pubLibp2p crypto.PubKey, privLibp2p crypto.PrivKey) *identity.KeyPair {
-
-	idKeyPair := &identity.KeyPair{}
-
-	// Turn pubLibp2p key into signal compatible public key (identity.key)
-	var pubSignal identity.Key
-	pubLibp2pBytes, err := pubLibp2p.Raw()
-	if err != nil {
-		log.Fatal("failed to get raw bytes from publibp2p")
-	}
-
-	var pubLibp2pBytesFix [32]byte
-	copy(pubLibp2pBytesFix[:], pubLibp2pBytes)
-	pubSignal = identity.NewKeyFromBytes(pubLibp2pBytesFix, 0)
-
-	// Turn privLibp2p key into signal compatible private key (ecc.ECPrivateKeyable)
-	var privSignal ecc.ECPrivateKeyable
-	privLibp2pBytes, err := privLibp2p.Raw()
-	if err != nil {
-		log.Fatal("failed to get raw bytes from privlibp2p")
-	}
-
-	var privLibp2pBytesFix [32]byte
-	copy(privLibp2pBytesFix[:], privLibp2pBytes)
-	privSignal = ecc.NewDjbECPrivateKey(privLibp2pBytesFix)
-
-	idKeyPair = identity.NewKeyPair(&pubSignal, privSignal)
-	return idKeyPair
-}
-
-//region CreateRaw
-func CreatePreKeyRaw() []byte {
-	preKeys, err := keyhelper.GeneratePreKeys(0, 10, serialize.NewJSONSerializer().PreKeyRecord)
-	if err != nil {
-		log.Fatal("failed to generate prekeys")
-	}
-
-	preKeyBytes := preKeys[0].Serialize()
-	writefile(preKeyBytes, prekeypath)
-
-	logger.Debug("Generated PreKey: ", preKeyBytes, "\nID: ", preKeys[0].ID(),
-		"\nPublicKey: ", preKeys[0].KeyPair().PublicKey().Serialize(),
-		"\nPrivateKey: ", preKeys[0].KeyPair().PrivateKey().Serialize())
-
-	return preKeyBytes
-}
-
-func CreateSignedPreKeyRaw(idKeyPair *identity.KeyPair) []byte {
-	signedPreKey, err := keyhelper.GenerateSignedPreKey(idKeyPair, 0, serialize.NewJSONSerializer().SignedPreKeyRecord)
-	if err != nil {
-		log.Fatal("failed to generate pre keys")
-	}
-
-	signedPreKeyBytes := signedPreKey.Serialize()
-	writefile(signedPreKeyBytes, sigprekeypath)
-
-	logger.Debug("Generated PreKey: ", signedPreKeyBytes, "\nID: ", signedPreKey.ID(),
-		"\nPublicKey: ", signedPreKey.KeyPair().PublicKey().Serialize(),
-		"\nPrivateKey: ", signedPreKey.KeyPair().PrivateKey().Serialize())
-
-	return signedPreKeyBytes
-}
-
-func CreateBundleRaw(pubLibp2p crypto.PubKey, privLibp2p crypto.PrivKey) RetrievableRaw {
-	pub, priv := SaveIDKeyPair(pubLibp2p, privLibp2p)
-	return RetrievableRaw{
-		Ids:                 CreateIDs(),
-		PreKey:              CreatePreKeyRaw(),
-		IdentityKeyPairPub:  pub,
-		IdentityKeyPairPriv: priv,
-		SignedPreKey:        CreateSignedPreKeyRaw(ConvertIDKeysLibp2pToSig(pubLibp2p, privLibp2p)),
-	}
-}
-
-//endregion CreateRaw
-
-//region ReadRaw
-func ReadPreKeyRaw() []byte {
-	if _, err := os.Stat(prekeypath); err != nil {
-		panic(err)
-	}
-
-	var preKeyBytes []byte
-	readfile(&preKeyBytes, prekeypath)
-	return preKeyBytes
-}
-
-func ReadSignedPreKeyRaw() []byte {
-	if _, err := os.Stat(sigprekeypath); err != nil {
-		panic(err)
-	}
-
-	var sigPreKeyBytes []byte
-	readfile(&sigPreKeyBytes, sigprekeypath)
-	return sigPreKeyBytes
-}
-
-func ReadBundleRaw() RetrievableRaw {
-	pub, priv := ReadIDKeyPairRaw()
-
-	return RetrievableRaw{
-		Ids:                 ReadIDs(),
-		PreKey:              ReadPreKeyRaw(),
-		IdentityKeyPairPub:  pub,
-		IdentityKeyPairPriv: priv,
-		SignedPreKey:        ReadSignedPreKeyRaw(),
-	}
-}
-
-// Returns a marshalled public and private key, if one is saved on file
-// ID keypairs are handled differently; The go implementations are saved / read
-// To get Signal compatible equivalent keys, use ConvertIDKeysLibp2pToSig
-func ReadIDKeyPairRaw() (pubBytes []byte, privBytes []byte) {
-	if _, err := os.Stat(idkeypubpath); err != nil {
-		panic(err)
-	} else if _, err = os.Stat(idkeyprivpath); err != nil {
-		panic(err)
-	}
-
-	readfile(&pubBytes, idkeypubpath)
-	readfile(&privBytes, idkeyprivpath)
-
-	return pubBytes, privBytes
-}
-
-//endregion ReadRaw
-
-//region Create
-// TODO dont hardcode devid
 func CreateIDs() IDs {
 	ids := IDs{
 		RegID: keyhelper.GenerateRegistrationID(),
@@ -257,9 +110,6 @@ func CreateIDs() IDs {
 	return ids
 }
 
-//endregion Create
-
-//region Read
 func ReadIDs() IDs {
 	if _, err := os.Stat(idspath); err != nil {
 		panic(err)
@@ -270,4 +120,184 @@ func ReadIDs() IDs {
 	return IDsFromGOB(idBytes)
 }
 
-//endregion Read
+func CreatePreKey() ([]byte, error) {
+	prekeys, err := keyhelper.GeneratePreKeys(0, 1, serialize.NewJSONSerializer().PreKeyRecord)
+	if err != nil {
+		return nil, err
+	}
+
+	prekey := prekeys[0].Serialize()
+	err = writefile(prekey, prekeypath)
+	if err != nil {
+		return nil, err
+	}
+
+	return prekey, nil
+}
+
+func ReadPreKey() ([]byte, error) {
+	var prekey []byte
+	if err := readfile(&prekey, prekeypath); err != nil {
+		return nil, err
+	}
+
+	return prekey, nil
+}
+
+func CreateSignedPreKey(idkeypair identity.KeyPair) ([]byte, error) {
+	signedprekey, err := keyhelper.GenerateSignedPreKey(&idkeypair, 0, serialize.NewJSONSerializer().SignedPreKeyRecord)
+	if err != nil {
+		return nil, err
+	}
+
+	signedprekeybytes := signedprekey.Serialize()
+	err = writefile(signedprekeybytes, sigprekeypath)
+	if err != nil {
+		return nil, err
+	}
+
+	return signedprekeybytes, nil
+}
+
+func ReadSignedPreKey() ([]byte, error) {
+	var signedprekey []byte
+	if err := readfile(&signedprekey, sigprekeypath); err != nil {
+		return nil, err
+	}
+
+	return signedprekey, nil
+}
+
+func CreateIdentityKeyPair() (pub []byte, priv []byte, err error) {
+	idkeypair, err := keyhelper.GenerateIdentityKeyPair()
+	if err != nil {
+		return nil, nil, err
+	}
+
+	pub = idkeypair.PublicKey().Serialize()
+	priv = bytehelper.ArrayToSlice(idkeypair.PrivateKey().Serialize())
+
+	if err = writefile(pub, idkeypubpath); err != nil {
+		return nil, nil, err
+	}
+	if err = writefile(priv, idkeyprivpath); err != nil {
+		return nil, nil, err
+	}
+
+	return pub, priv, err
+}
+
+func ReadIdentityKeyPair() (pub []byte, priv []byte, err error) {
+	if err = readfile(&pub, idkeypubpath); err != nil {
+		return nil, nil, err
+	}
+
+	if err = readfile(&priv, idkeyprivpath); err != nil {
+		return nil, nil, err
+	}
+
+	return pub, priv, nil
+}
+
+func ReadIdentityKeyPair2() identity.KeyPair {
+	var pub, priv []byte
+	readfile(&pub, idkeypubpath)
+	readfile(&priv, idkeyprivpath)
+
+	var readpubfix, readprivfix [32]byte
+	copy(readpubfix[:], pub[1:])
+	copy(readprivfix[:], priv)
+
+	retpub := identity.NewKeyFromBytes(readpubfix, 0)
+	retpriv := ecc.NewDjbECPrivateKey(readprivfix)
+	return *identity.NewKeyPair(&retpub, retpriv)
+}
+
+func CreateBundleRaw() (RetrievableRaw, error) {
+	ids := CreateIDs()
+
+	prekey, err := CreatePreKey()
+	if err != nil {
+		return RetrievableRaw{}, err
+	}
+
+	pub, priv, err := CreateIdentityKeyPair()
+	if err != nil {
+		return RetrievableRaw{}, err
+	}
+
+	idkeypair := ReadIdentityKeyPair2()
+	signedprekey, err := CreateSignedPreKey(idkeypair)
+	if err != nil {
+		return RetrievableRaw{}, err
+	}
+
+	return RetrievableRaw{
+		Ids:                 ids,
+		PreKey:              prekey,
+		SignedPreKey:        signedprekey,
+		IdentityKeyPairPub:  pub,
+		IdentityKeyPairPriv: priv,
+	}, nil
+}
+
+func ReadBundleRaw() (RetrievableRaw, error) {
+	ids := ReadIDs()
+
+	prekey, err := ReadPreKey()
+	if err != nil {
+		return RetrievableRaw{}, err
+	}
+
+	pub, priv, err := ReadIdentityKeyPair()
+	if err != nil {
+		return RetrievableRaw{}, err
+	}
+
+	signedprekey, err := ReadSignedPreKey()
+	if err != nil {
+		return RetrievableRaw{}, err
+	}
+
+	return RetrievableRaw{
+		Ids:                 ids,
+		PreKey:              prekey,
+		SignedPreKey:        signedprekey,
+		IdentityKeyPairPub:  pub,
+		IdentityKeyPairPriv: priv,
+	}, nil
+}
+
+func ReadBundle() (Retrievable, error) {
+	ids := ReadIDs()
+
+	prekeybytes, err := ReadPreKey()
+	if err != nil {
+		return Retrievable{}, err
+	}
+	prekey, err := record.NewPreKeyFromBytes(prekeybytes, serialize.NewJSONSerializer().PreKeyRecord)
+	if err != nil {
+		return Retrievable{}, err
+	}
+
+	idkeypair := ReadIdentityKeyPair2()
+	if err != nil {
+		return Retrievable{}, err
+	}
+
+	signedprekeybytes, err := ReadSignedPreKey()
+	if err != nil {
+		return Retrievable{}, err
+	}
+	signedprekey, err := record.NewSignedPreKeyFromBytes(signedprekeybytes, serialize.NewJSONSerializer().SignedPreKeyRecord)
+	if err != nil {
+		return Retrievable{}, err
+	}
+
+	return Retrievable{
+		Ids:             ids,
+		PreKey:          *prekey,
+		SignedPreKey:    *signedprekey,
+		IdentityKeyPair: idkeypair,
+	}, nil
+}
